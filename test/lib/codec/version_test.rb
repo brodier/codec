006655c0d0967ac51f869fb020abca98c8cbb617
Bernard Rodier <bernard.rodier@gmail.com>
2013-08-06 15:48:57 +0200

    adding testfiles

---
+++ test/lib/codec/version_test.rb
@@ -0,0 +1,10 @@
+require_relative '../../test_helper'
+ 
+describe Codec do
+ 
+  it "must be defined" do
+    Codec::VERSION.wont_be_nil
+  end
+ 
+end
+
---
+++ test/test_helper.rb
@@ -0,0 +1,3 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require File.expand_path('../../lib/codec.rb', __FILE__)
