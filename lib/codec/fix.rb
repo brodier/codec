c0d0555ef55c11f052226c5d9c0aa426d646b190
Bernard Rodier <bernard.rodier@gmail.com>
2013-10-15 15:22:18 +0200

    adding class for autoconverting EBCDIC to ASCII

--- lib/codec/fix.rb
+++ lib/codec/fix.rb
@@ -111,6 +111,7 @@ module Codec
     end    
   end
   
+  
   class String < Base
     def build_field(buf,length)
       f = Field.new(@id)
@@ -143,5 +144,46 @@ module Codec
       end
       return out
     end 
+  end
+
+  # Class Auto Converting Ebcdic to ASCII for number and string
+    EBCDIC_2_ASCII = ["000102039C09867F978D8E0B0C0D0E0F101112139D8508871819928F1C1D1E1F"+
+      "80818283840A171B88898A8B8C050607909116939495960498999A9B14159E1A"+
+      "20A0A1A2A3A4A5A6A7A8D52E3C282B7C26A9AAABACADAEAFB0B121242A293B5E"+
+      "2D2FB2B3B4B5B6B7B8B9E52C255F3E3FBABBBCBDBEBFC0C1C2603A2340273D22"+
+      "C3616263646566676869C4C5C6C7C8C9CA6A6B6C6D6E6F707172CBCCCDCECFD0"+
+      "D17E737475767778797AD2D3D45BD6D7D8D9DADBDCDDDEDFE0E1E2E3E45DE6E7"+
+      "7B414243444546474849E8E9EAEBECED7D4A4B4C4D4E4F505152EEEFF0F1F2F3"+
+      "5C9F535455565758595AF4F5F6F7F8F930313233343536373839FAFBFCFDFEFF"].pack("H*")
+  
+  class Numace < Numstr
+    def build_field(buf,length)
+      f = Field.new(@id)
+      
+      data = ""
+      # if buf to decode is in EBCDIC then convert buf in ASCII
+      if ( buf[0,length].unpack("C*").select{|c| c >= 128}.size > 0)
+        buf[0,length].unpack("C*").each { |c| data += EBCDIC_2_ASCII[c] }
+      else
+        data = buf[0,length]
+      end
+      f.set_value(data.to_i)
+      return f
+    end
+  end
+
+  class Strace < String
+    def build_field(buf,length)
+      f = Field.new(@id)
+      data = ""
+      # if buf to decode is in EBCDIC then convert buf in ASCII
+      if ( buf[0,length].unpack("C*").select{|c| c >= 128}.size > 0)
+        buf[0,length].unpack("C*").each { |c| data += EBCDIC_2_ASCII[c] }
+      else
+        data = buf[0,length]
+      end
+      f.set_value(data)
+      return f
+    end
   end
 end
